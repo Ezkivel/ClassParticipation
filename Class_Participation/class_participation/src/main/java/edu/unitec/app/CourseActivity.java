7c76c0a5e8edcdbf12f29e5b01e3d2d650411bb3
Henry23 <gabriel_23hgap@unitec.edu>
2013-12-01 01:50:12 -0600

    cambios

---
+++ Class_Participation/class_participation/src/main/java/edu/unitec/app/CourseActivity.java
@@ -0,0 +1,16 @@
+package edu.unitec.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Henry on 12-01-13.
+ */
+public class CourseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}

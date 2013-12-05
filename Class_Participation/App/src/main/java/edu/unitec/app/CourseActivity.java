bd78e5ba036727f0fb561029ba4d71da61a1404b
Henry23 <gabriel_23hgap@unitec.edu>
2013-12-05 10:10:12 -0600

    cambios

--- Class_Participation/App/src/main/java/edu/unitec/app/CourseActivity.java
+++ Class_Participation/App/src/main/java/edu/unitec/app/CourseActivity.java
@@ -36,6 +36,7 @@ public class CourseActivity extends Activity {
                 EditText course_code = (EditText) findViewById(R.id.course_code);
                 EditText course_name = (EditText) findViewById(R.id.course_name);
                 EditText course_description = (EditText) findViewById(R.id.course_description);
+
                 String course_cod = course_code.getText().toString();
                 String course_nam = course_name.getText().toString();
                 String course_desp = course_description.getText().toString();
@@ -45,20 +46,23 @@ public class CourseActivity extends Activity {
                 Log.i("Info3",course_desp);
 
                 if( !course_cod.isEmpty() && !course_nam.isEmpty() && !course_desp.isEmpty() ){
-                    /*Course course = new Course();
                     DatabaseHandler admin = new DatabaseHandler(this);
-                    SQLiteDatabase bd = admin.getWritableDatabase();
-                    course.setCourseCode(course_code.getText().toString());
-                    course.setCourseName(course_name.getText().toString());
-                    course.setCourseDescription(course_description.getText().toString());
-                    admin.addCourse(course);
-                    bd.close();
-                    Context context = getApplicationContext();
-                    CharSequence text = "Success!!";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();/*
-                    //still an error 
+                    if( admin != null ){
+                        Course course = new Course();
+                        course.setCourseId(1);
+                        course.setCourseCode(course_cod);
+                        course.setCourseName(course_nam);
+                        course.setCourseDescription(course_desp);
+                        admin.addCourse(course);
+                        Context context = getApplicationContext();
+                        CharSequence text = "Success!!";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        course_code.setText("");
+                        course_name.setText("");
+                        course_description.setText("");
+                    }
                 }else {
                     Context context = getApplicationContext();
                     CharSequence text = "ERROR!!";
